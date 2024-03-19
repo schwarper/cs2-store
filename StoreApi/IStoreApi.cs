aca1ef47dac76179166c7233803a19d286f529a7
schwarper <75811921+schwarper@users.noreply.github.com>
2024-03-19 04:57:21 +0300

    0.0.4
    - Added api files

---
+++ StoreApi/IStoreApi.cs
@@ -0,0 +1,20 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+using static StoreApi.Store;
+
+namespace StoreApi;
+
+public interface IStoreApi
+{
+    public static readonly PluginCapability<IStoreApi?> Capability = new("store:api");
+
+    public void SetPlayerCredits(CCSPlayerController player, int value);
+    public void GivePlayerCredits(CCSPlayerController player, int amount);
+    public int GetPlayerCredits(CCSPlayerController player);
+    public bool IsPlayerVip(CCSPlayerController player);
+    public void RegisterType(string type, Action mapStart, Func<CCSPlayerController, Store_Item, bool> equip, Func<CCSPlayerController, Store_Item, bool> unequip, bool equipable, bool? alive = false);
+    public Store_Item? GetItem(string uniqueId);
+    public bool PlayerHasItem(CCSPlayerController player, string uniqueId);
+    public bool PlayerUsingItem(CCSPlayerController player, string uniqueId);
+    public bool ItemIsInJson(string type, string uniqueId);
+}
---
+++ StoreApi/Store.cs
@@ -0,0 +1,51 @@
+﻿using CounterStrikeSharp.API.Core;
+using Timer = CounterStrikeSharp.API.Modules.Timers.Timer;
+
+namespace StoreApi;
+
+public abstract class Store
+{
+    public class Store_Player
+    {
+        public required ulong SteamID { get; set; }
+        public required string PlayerName { get; set; }
+        public required int Credits { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public DateTime DateOfLastJoin { get; set; }
+    }
+    public class Store_PlayerItem
+    {
+        public required ulong SteamID { get; set; }
+        public required string Type { get; set; }
+        public required int Price { get; set; }
+        public string UniqueId { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Flag { get; set; } = string.Empty;
+        public int Slot { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+    }
+    public class Store_Item
+    {
+        public required string Name { get; set; }
+        public required string Type { get; set; }
+        public required int Price { get; set; }
+        public string UniqueId { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Flag { get; set; } = string.Empty;
+        public int Slot { get; set; }
+        public bool Enable { get; set; }
+    }
+    public class Store_Item_Types
+    {
+        public required string Type;
+        public required Action MapStart;
+        public required Func<CCSPlayerController, Store_Item, bool> Equip;
+        public required Func<CCSPlayerController, Store_Item, bool> Unequip;
+        public required bool Equipable;
+        public bool? Alive;
+    }
+    public class Player
+    {
+        public Timer? CreditIntervalTimer { get; set; }
+    }
+}
