62f29a4da7a25da6ac847d1501e3c2474fa89ad1
schwarper <75811921+schwarper@users.noreply.github.com>
2024-03-19 05:01:17 +0300

    0.0.4
    - Added sample file for api system.

---
+++ Store_TestModule/Store_TestModule.cs
@@ -0,0 +1,40 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using StoreApi;
+using static StoreApi.Store;
+
+namespace Store_TestModule;
+
+public class Store_TestModule : BasePlugin
+{
+    public override string ModuleName { get; } = "Store Module [Test]";
+    public override string ModuleVersion { get; } = "0.0.1";
+
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        var storeApi = IStoreApi.Capability.Get();
+
+        if (storeApi == null)
+        {
+            return;
+        }
+
+        storeApi.RegisterType("test", OnMapStart, Equip, Unequip, true, true);
+    }
+
+    private void OnMapStart()
+    {
+    }
+
+    private bool Equip(CCSPlayerController player, Store_Item item)
+    {
+        Server.PrintToChatAll($"Player {player.PlayerName} equipped {item.Name}");
+        return true;
+    }
+
+    private bool Unequip(CCSPlayerController player, Store_Item item)
+    {
+        Server.PrintToChatAll($"Player {player.PlayerName} unequipped {item.Name}");
+        return true;
+    }
+}
