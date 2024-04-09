f810d895e92e958e090ebbf4c99989ad7820cf60
schwarper <75811921+schwarper@users.noreply.github.com>
2024-04-09 17:57:35 +0300

    Update cs2-store.cs

--- Store/src/cs2-store.cs
+++ Store/src/cs2-store.cs
@@ -8,7 +8,7 @@ namespace Store;
 public partial class Store : BasePlugin, IPluginConfig<StoreConfig>
 {
     public override string ModuleName => "Store";
-    public override string ModuleVersion => "0.1.2";
+    public override string ModuleVersion => "0.1.2a";
     public override string ModuleAuthor => "schwarper & xshadowbringer";
 
     public override void Load(bool hotReload)
