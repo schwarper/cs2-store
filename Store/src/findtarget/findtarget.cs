bd4db6960f0b5868b3db026cd8363d05fc87c777
root <root@Andr>
2024-04-30 01:26:35 +0100

    Namespace Declaration | Using Directives | Comments | Variable Naming | Consistency

--- Store/src/findtarget/findtarget.cs
+++ Store/src/findtarget/findtarget.cs
@@ -1,68 +1,64 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Commands.Targeting;
-using static Store.Store;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Store;
-
-public static class FindTarget
+namespace Store
 {
-    public static (List<CCSPlayerController> players, string targetname) Find
-        (
-            CommandInfo command,
-            int minArgCount,
-            bool singletarget,
-            bool ignoreMessage = false
-        )
+    public static class FindTarget
     {
-        if (command.ArgCount < minArgCount)
+        public static (List<CCSPlayerController> players, string targetName) Find(CommandInfo command, int minArgCount, bool singleTarget, bool ignoreMessage = false)
         {
-            return (new List<CCSPlayerController>(), string.Empty);
-        }
-
-        TargetResult targetresult = command.GetArgTargetResult(1);
-
-        if (targetresult.Players.Count == 0)
-        {
-            if (!ignoreMessage)
+            if (command.ArgCount < minArgCount)
             {
-                command.ReplyToCommand(Instance.Config.Tag + Instance.Localizer["No matching client"]);
+                return (new List<CCSPlayerController>(), string.Empty);
             }
 
-            return (new List<CCSPlayerController>(), string.Empty);
-        }
-        else if (singletarget && targetresult.Players.Count > 1)
-        {
-            command.ReplyToCommand(Instance.Config.Tag + Instance.Localizer["More than one client matched"]);
-            return (new List<CCSPlayerController>(), string.Empty);
-        }
+            TargetResult targetResult = command.GetArgTargetResult(1);
 
-        string targetname;
-
-        if (targetresult.Players.Count == 1)
-        {
-            targetname = targetresult.Players.Single().PlayerName;
-        }
-        else
-        {
-            Target.TargetTypeMap.TryGetValue(command.GetArg(1), out TargetType type);
-
-            targetname = type switch
+            if (targetResult.Players.Count == 0)
             {
-                TargetType.GroupAll => Instance.Localizer["all"],
-                TargetType.GroupBots => Instance.Localizer["bots"],
-                TargetType.GroupHumans => Instance.Localizer["humans"],
-                TargetType.GroupAlive => Instance.Localizer["alive"],
-                TargetType.GroupDead => Instance.Localizer["dead"],
-                TargetType.GroupNotMe => Instance.Localizer["notme"],
-                TargetType.PlayerMe => targetresult.Players.First().PlayerName,
-                TargetType.TeamCt => Instance.Localizer["ct"],
-                TargetType.TeamT => Instance.Localizer["t"],
-                TargetType.TeamSpec => Instance.Localizer["spec"],
-                _ => targetresult.Players.First().PlayerName
-            };
-        }
+                if (!ignoreMessage)
+                {
+                    command.ReplyToCommand(Instance.Config.Tag + Instance.Localizer["No matching client"]);
+                }
 
-        return (targetresult.Players, targetname);
+                return (new List<CCSPlayerController>(), string.Empty);
+            }
+            else if (singleTarget && targetResult.Players.Count > 1)
+            {
+                command.ReplyToCommand(Instance.Config.Tag + Instance.Localizer["More than one client matched"]);
+                return (new List<CCSPlayerController>(), string.Empty);
+            }
+
+            string targetName;
+
+            if (targetResult.Players.Count == 1)
+            {
+                targetName = targetResult.Players.Single().PlayerName;
+            }
+            else
+            {
+                Target.TargetTypeMap.TryGetValue(command.GetArg(1), out TargetType type);
+
+                targetName = type switch
+                {
+                    TargetType.GroupAll => Instance.Localizer["all"],
+                    TargetType.GroupBots => Instance.Localizer["bots"],
+                    TargetType.GroupHumans => Instance.Localizer["humans"],
+                    TargetType.GroupAlive => Instance.Localizer["alive"],
+                    TargetType.GroupDead => Instance.Localizer["dead"],
+                    TargetType.GroupNotMe => Instance.Localizer["notme"],
+                    TargetType.PlayerMe => targetResult.Players.First().PlayerName,
+                    TargetType.TeamCt => Instance.Localizer["ct"],
+                    TargetType.TeamT => Instance.Localizer["t"],
+                    TargetType.TeamSpec => Instance.Localizer["spec"],
+                    _ => targetResult.Players.First().PlayerName
+                };
+            }
+
+            return (targetResult.Players, targetName);
+        }
     }
 }
