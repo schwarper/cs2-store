ecf49ca3177d7ca222747f634003f89ca9f5eaf0
AiDN™ <45371311+originalaidn@users.noreply.github.com>
2024-08-02 15:12:48 +0200

    updating menu, adding more if to ALL, CT or T checks
    adding these 3 if
    if no slot 1 OR 2 OR 3, than dont add it into menu

--- Store/src/menu/menu.cs
+++ Store/src/menu/menu.cs
@@ -117,6 +117,19 @@ public static class Menu
 
             foreach (int Slot in new[] { 1, 2, 3 })
             {
+                if (Slot == 1 && !playerSkinItems.Any(p => p.Value.TryGetValue("slot", out string? slot) && slot == "1"))
+                {
+                    continue;
+                }
+                if (Slot == 2 && !playerSkinItems.Any(p => p.Value.TryGetValue("slot", out string? slot) && slot == "2"))
+                {
+                    continue;
+                }
+                if (Slot == 3 && !playerSkinItems.Any(p => p.Value.TryGetValue("slot", out string? slot) && slot == "3"))
+                {
+                    continue;
+                }
+                
                 if (inventory && !playerSkinItems.Any(item => Item.PlayerHas(player, item.Value["type"], item.Value["uniqueid"], false)))
                 {
                     continue;
