76f1faf2ff31af84d93183405d53370a5241102d
schwarper <75811921+schwarper@users.noreply.github.com>
2024-10-13 19:02:08 +0300

    v1.5

--- Store/src/cs2-store.cs
+++ Store/src/cs2-store.cs
@@ -9,7 +9,7 @@ namespace Store;
 public class Store : BasePlugin, IPluginConfig<Item_Config>
 {
     public override string ModuleName => "Store";
-    public override string ModuleVersion => "1.4";
+    public override string ModuleVersion => "1.5";
     public override string ModuleAuthor => "schwarper";
 
     public Item_Config Config { get; set; } = new Item_Config();
